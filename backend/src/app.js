9c9b4193389cc11d86e129ddb76c1999f62823d0
Naeem-khndkar55 <naeemkhandakar5@gmail.com>
2025-05-24 22:26:16 +0600

    folder and file structure design done

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
-const { notFound, errorHandler } = require("./middleware/error.middleware");
+
 const connectDB = require("./config/db.config");
-const authRoutes = require("./routes/auth.routes");
 
 require("dotenv").config();
 const app = express();
@@ -15,18 +14,5 @@ connectDB();
 app.use(helmet());
 app.use(cors({}));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// Routes
-app.use("/api/auth", authRoutes);
-
-// Health check route
-app.get("/api/health", (req, res) => {
-  res.status(200).json({ status: "OK" });
-});
-
-// Error handling middleware
-app.use(notFound);
-app.use(errorHandler);
 
 module.exports = app;

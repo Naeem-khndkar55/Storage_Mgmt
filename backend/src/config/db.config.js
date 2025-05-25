70ecd67dd181b0526392c51718d2e2576984d4a9
Naeem-khndkar55 <naeemkhandakar5@gmail.com>
2025-05-25 17:00:11 +0600

    final commit

--- backend/src/config/db.config.js
+++ backend/src/config/db.config.js
@@ -1,5 +1,8 @@
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
+// Load environment variables
+dotenv.config();
 const connectDB = async () => {
   const uri = process.env.MONGO_URI;
 
